1525f2ad215bbfdaa7e775c963714d747b011ad4
Your Name <you@example.com>
2025-07-28 16:02:55 +0000

    More unit tests

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
 public static void main(String[] args) {
 // Prints "Hello, World" to the terminal window.
-System.out.println("Hello, World, again!");
+HelloWorldHelper helper = new HelloWorldHelper ();
+System.out.println(helper.useEnglish());
 }
 }
 
